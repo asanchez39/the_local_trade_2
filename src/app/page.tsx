8c8b0e7d0b270b7d8f4c1bea6682be0fe6192161
Your Name <youremail@example.com>
2025-02-12 10:06:31 -0800

    Add next.config.js and update import paths in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image"
-import EventList from "../../components/EventList"
-import EmailForm from "../../components/EmailForm"
+import EventList from "@/components/EventList"
+import EmailForm from "@/components/EmailForm"
 
 export default function Home() {
   return (
